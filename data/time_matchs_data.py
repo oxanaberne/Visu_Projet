9e8722499576d6e54c1fab291ebb800e10dc6d0e
oxanaberne <oxana.berne@polymtl.ca>
2024-04-22 18:32:56 -0400

    Ajout données réels pour visu time_matchs

---
+++ data/time_matchs_data.py
@@ -0,0 +1,9 @@
+data = {
+        'Match 1': { 'Buts pour' : [4, 58], 'Buts contre' : [], 'Changements' : [46, 61, 62, 77, 91], 'Cartons jaunes' : [29, 45] },
+        'Match 2': { 'Buts pour' : [], 'Buts contre' : [55], 'Changements' : [67,67,67,80,84], 'Cartons jaunes' : [] },
+        'Match 3': { 'Buts pour' : [], 'Buts contre' : [42,73,75,88], 'Changements' : [64,65,65,84,84], 'Cartons jaunes' : [45,72] },
+        'Match 4': { 'Buts pour' : [86], 'Buts contre' : [4], 'Changements' : [64,65,72,73,79,116], 'Cartons jaunes' : [82,90,95] },
+        'Match 5': { 'Buts pour' : [90, 120], 'Buts contre' : [71], 'Changements' : [45,46,46,73,86], 'Cartons jaunes' : [16,33,37,43,92, 111,120] },
+        'Match 6': { 'Buts pour' : [65], 'Buts contre' : [], 'Changements' : [61,61,80,90,90], 'Cartons jaunes' : [] },
+        'Match 7': { 'Buts pour' : [62, 81], 'Buts contre' : [38], 'Changements' : [70,70,87,88,92], 'Cartons jaunes' : [54,87,97] }
+        }
