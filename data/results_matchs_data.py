eff28cb7fb61af092c15f8138531626c3530aad0
oxanaberne <oxana.berne@polymtl.ca>
2024-04-23 10:39:46 -0400

    retrait ligne inutile + ajout données

---
+++ data/results_matchs_data.py
@@ -0,0 +1,9 @@
+data = {
+        'Match 1': 'W',
+        'Match 2': 'L',
+        'Match 3': 'L',
+        'Match 4': 'W', # La Côte D'Ivoire a gagné sur les penalty
+        'Match 5': 'W',
+        'Match 6': 'W',
+        'Match 7': 'W'
+        }
