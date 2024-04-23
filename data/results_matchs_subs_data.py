afb8e4a4339ef26acd180205c9fc8226157828fd
oxanaberne <oxana.berne@polymtl.ca>
2024-04-23 13:26:23 -0400

    ajout visu 10 pour resultats de matchs par remplacants

---
+++ data/results_matchs_subs_data.py
@@ -0,0 +1,9 @@
+data = {
+        'Match 1': {'Results': 'W', 'Players': ['Karim Konaté', 'Nicolas Pépé', 'Idrissa Doumbia', 'Willy Boly', 'Serge Aurier']},
+        'Match 2': {'Results': 'L', 'Players': ['Wilfried Singo', 'Nicolas Pépé', 'Jonathan Bamba', 'Oumar Diakité', 'Karim Konaté']},
+        'Match 3': {'Results': 'L', 'Players': ['Jean-Philippe Krasso', 'Karim Konaté', 'Jeremie Boga', 'Simon Adingra', 'Max Gradel']},
+        'Match 4': {'Results': 'W', 'Players': ['Simon Adingra', 'Nicolas Pépé', 'Sébastien Haller', 'Franck Kessié', 'Christian Kouamé', 'Wilfried Singo']}, # La Côte D'Ivoire a gagné sur les penalty
+        'Match 5': {'Results': 'W', 'Players': ['Wilfried Singo', 'Sébastien Haller', 'Willy Boly', 'Oumar Diakité', 'Simon Adingra']},
+        'Match 6': {'Results': 'W', 'Players': ['Jean Thierry Lazare', 'Ibrahim Sangaré', 'Jonathan Bamba', 'Jean-Philippe Krasso', 'Jeremie Boga']},
+        'Match 7': {'Results': 'W', 'Players': ['Wilfried Singo', 'Oumar Diakité', 'Jean-Philippe Krasso', 'Ibrahim Sangaré', 'Jean Thierry Lazare']},
+        }
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pathlib
 import graphs.players_performance as pp
 import graphs.subs_performance as sp
+import graphs.results_matchs_subs as rm
 import graphs.statistiques_matchs as sm
 import graphs.time_matchs as tm
 from fastapi import FastAPI, Request # type: ignore
@@ -45,6 +46,7 @@ async def get_charts(request: Request):
     plot_sub_mid_global = sp.sub_mid_perf_global() #TODO
     plot_sub_def_matchs = sp.sub_defenders_perf_by_matchs()
     plot_sub_def_global = sp.sub_defenders_perf_global() #TODO
+    plot_results_subs = rm.draw_results_per_player()
 
     # Partie 4 :Analyse des matchs dans le temps
     plot_times_matchs = tm.decisives_actions_matchs()
@@ -75,6 +77,7 @@ async def get_charts(request: Request):
             "plot_sub_mid_global": plot_sub_mid_global,
             "plot_sub_def_matchs": plot_sub_def_matchs,
             "plot_sub_def_global": plot_sub_def_global,
+            "plot_results_subs": plot_results_subs,
             # Partie 4
             "plot_times_matchs": plot_times_matchs
         }
